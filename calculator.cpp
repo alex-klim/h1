75564d61f056da17dd519fcc243c82a64739ae9b
Oleksandr Klymenko <olek.klymenko@gmail.com>
2018-11-22 14:25:13 +0200

    add division operation
    Function doesn't handle right bad arguments situation.
    Straight comparison of doubles can cause issues and lead to wrong
    results.
    
    Signed-off-by: Oleksandr Klymenko <olek.klymenko@gmail.com>

--- calculator.cpp
+++ calculator.cpp
@@ -14,3 +14,14 @@ int Calculator::Mul (double a, double b)
 {
     return a * b + 0.5;
 }
+
+int Calculator::Div (double a, double b)
+{
+    if(b != 0.0)
+    {
+        return a / b + 0.5;
+    } else {
+        // bad argument handling
+        return 0;
+    }
+}
